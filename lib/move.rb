165fb9612b5adc63ee3c6dc5048fb4e82b2f079f
Chamaeleonidae Na <nanalau315@hotmail.com>
2020-09-18 07:33:01 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,9 +8,9 @@ end
 
 # code your input_to_index and move method here!
 
-def input_to_index(input)
-  puts input.to_i - 1
-  input.to_i - 1
+def input_to_index(index)
+  puts index.to_i - 1
+  index.to_i - 1
 end
 
 
